8c7ddd4efd98ff2a4393c934818ce91e41b6ac73
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-09-24 09:21:31 +0530

    Binary Search-2

--- src/searching/BinarySearch_OrderAgnostic.java
+++ src/searching/BinarySearch_OrderAgnostic.java
@@ -1,7 +1,33 @@
 package searching;
-
+//When sorted order is not known
 public class BinarySearch_OrderAgnostic {
     public static void main(String[] args) {
+        int[] arr={7,6,5,4,3,2,1};
+        int target=7;
+        System.out.println(order(arr,target));
+    }
 
+    static int order(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        boolean isAsc=arr[start]<arr[end];
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target)
+                return mid;
+            if(isAsc){
+                if(target<arr[mid])
+                    end=mid-1;
+                else
+                    start=mid+1;
+            }
+            else{
+                if(target>arr[mid])
+                    end=mid-1;
+                else
+                    start=mid+1;
+            }
+        }
+        return -1;
     }
 }
