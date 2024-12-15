93acd3e717700bdda00fc54feba006d893f0d5f2
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-12-15 10:19:12 +0530

    _

--- src/Sorting/Quick_Sort.java
+++ src/Sorting/Quick_Sort.java
@@ -1,2 +1,20 @@
-package Sorting;public class Quick_Sort {
+package Sorting;
+
+public class Quick_Sort {
+    public static void main(String[] args) {
+
+    }
 }
+/*
+-> 5,4,3,2,1
+->After first pass: (4 as pivot element)
+    1,3,2,4,5
+
+->After first pass, all the elements less than pivot element will be left hand side of the pivot.
+    All the elements greater than pivot will be at right side of the pivot.
+->Pivot can be any element in the array.
+->After every pass, we are putting pivot at the correct position.
+->
+->Time Complexity:   Space Complexity:
+
+ */
