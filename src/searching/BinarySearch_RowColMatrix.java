9acf588b54eafbcba9619272dd1860c87143a419
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-12-15 10:18:24 +0530

    Binary Search for 2d Arrays

--- src/searching/BinarySearch_RowColMatrix.java
+++ src/searching/BinarySearch_RowColMatrix.java
@@ -2,7 +2,13 @@ package searching;
 
 public class BinarySearch_RowColMatrix {
     public static void main(String[] args) {
-
+        int[][] arr={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        int target=7;
+        search(arr,target);
     }
     static int[] search(int[][] matrix,int target){
         int r=0;
